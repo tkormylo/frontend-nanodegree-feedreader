1194f5821b9bdd97b8d73008ae6ffe776aed3940
Thomas K <tkormylo@gmail.com>
2015-12-14 22:18:09 -0700

    Tests 1-14 Complete and working.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -114,6 +114,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -121,6 +122,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('should have a least a single entry element when loadFeed completes', function() {
+            expect($('.feed').children().length).not.toBe(0);
+        });
+    });
+
+
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
